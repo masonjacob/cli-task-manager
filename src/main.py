b5f4a95d9f9f094cd08ede3ea6e4eaf64aafa21d
Mason Jacob <masonjacob@uri.edu>
2022-10-14 07:29:57 -0400

    Initial Commit

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from struct import calcsize
+from icalendar import Calendar, Event
+from datetime import datetime
+from pytz import UTC # timezone
+
+cal = Calendar()
+f = open('example.ics', 'wb')
+f.read()
+print(cal)
